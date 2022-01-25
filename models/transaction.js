ce5d4327a50f32849ac997289c146bac20788484
vanessaconti <vconti901@aol.com>
2022-01-25 01:10:02 -0500

    added to model

--- models/transaction.js
+++ models/transaction.js
@@ -2,51 +2,61 @@ module.exports = [
   {
     date: "April 1",
     source: "Income",
+    isThisCredit: true,
     amount: 1000,
   },
   {
     date: "April 1",
     source: "Taxes",
+    isThisCredit: true,
     amount: -300,
   },
   {
     date: "April 1",
     source: "Retirement",
+    isThisCredit: true,
     amount: -200,
   },
   {
     date: "April 1",
     source: "Savings",
+    isThisCredit: true,
     amount: -100,
   },
   {
     date: "April 1",
     source: "Credit Card Payment",
+    isThisCredit: true,
     amount: -100,
   },
   {
     date: "April 5",
     source: "Monthly Birthday Money from Aunt Tilda",
+    isThisCredit: true,
     amount: 20,
   },
   {
     date: "April 5",
     source: "Coffee",
+    isThisCredit: true,
     amount: -4,
   },
   {
     date: "April 5",
     source: "Internet",
+    isThisCredit: true,
     amount: -100,
   },
   {
     date: "April 3",
     source: "Groceries",
+    isThisCredit: true,
     amount: -76,
   },
   {
     date: "April 3",
     source: "Pet Food",
+    isThisCredit: true,
     amount: -7,
   },
 ];
